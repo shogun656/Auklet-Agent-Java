85763ade5ee49a6bf0f5931941451e6f628bd943
nikhilpalaskar <npalaska@umflint.edu>
2018-10-01 16:57:45 -0400

    rename device to device_id in messagePack

--- src/main/java/io/auklet/agent/Messages.java
+++ src/main/java/io/auklet/agent/Messages.java
@@ -22,7 +22,7 @@ public class Messages {
             map.put("publicIP", util.getIpAddress());
             map.put("systemMetrics", util.getSystemMetrics());
             map.put("agentVersion", 0.0);
-            map.put("device", Device.getClient_id());
+            map.put("device_id", Device.getClient_id());
 
             System.out.println("message pack: " + map);
 
