fad403bc5a99e7b8cac522fd6e47452b6d80b819
Ron Jenkins Jr <ronald.j.jenkins@gmail.com>
2018-10-05 17:31:04 -0400

    Fix path to version file

--- .devops/deploy.sh
+++ .devops/deploy.sh
@@ -5,8 +5,8 @@ if [[ "$1" == "" ]]; then
   exit 1
 fi
 ENVDIR=$1
-VERSION="$(cat VERSION)"
-VERSION_SIMPLE=$(cat VERSION | xargs | cut -f1 -d"+")
+VERSION="$(cat ~/.version)"
+VERSION_SIMPLE=$(cat ~/.version | xargs | cut -f1 -d"+")
 export TIMESTAMP="$(date --rfc-3339=seconds | sed 's/ /T/')"
 
 sed -i -e "s/theVersion=.*/theVersion=$VERSION_SIMPLE/" gradle.properties
