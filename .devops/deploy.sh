3ea27e42e49c91347b2f8cb1e0f6cb46800acfe3
Ron Jenkins Jr <ronald.j.jenkins@gmail.com>
2018-10-05 17:26:36 -0400

    Define version at deploy-time

--- .devops/deploy.sh
+++ .devops/deploy.sh
@@ -9,4 +9,5 @@ VERSION="$(cat VERSION)"
 VERSION_SIMPLE=$(cat VERSION | xargs | cut -f1 -d"+")
 export TIMESTAMP="$(date --rfc-3339=seconds | sed 's/ /T/')"
 
+sed -i -e "s/theVersion=.*/theVersion=$VERSION_SIMPLE/" gradle.properties
 gradle bintrayUpload
