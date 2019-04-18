2ce60e1b9622783dc38d65993d68ac1e750371c8
Tyler Wiersing <tyler.wiersing@gmail.com>
2019-04-18 04:13:24 -0400

    codeclimate

--- src/main/java/io/auklet/Datapoint.java
+++ src/main/java/io/auklet/Datapoint.java
@@ -4,7 +4,6 @@ import java.math.BigInteger;
 import io.auklet.AukletException;
 import org.msgpack.core.MessageBufferPacker;
 import org.msgpack.core.MessagePack;
-import org.msgpack.core.MessagePacker;
 import net.jcip.annotations.GuardedBy;
 
 import java.io.*;
@@ -14,8 +13,9 @@ import java.util.*;
 public class Datapoint {
 
     @GuardedBy("itself") protected final MessageBufferPacker msgpack = MessagePack.newDefaultBufferPacker();
-    private final String dataType = "value";
-    public byte[] dataValue;
+    private static final String dataType = "value";
+    private static final String assemblyFailure = "Could not assemble datapoint message";
+    public static byte[] dataValue;
 
     /**
      * <p>Initializes a Datapoint class which packs up the passed data into messagepack byte arrays</p>
@@ -30,7 +30,7 @@ public class Datapoint {
                 this.msgpack.packMapHeader(1)
                         .packString(this.dataType).packBoolean(data);
             } catch (IOException e) {
-                throw new AukletException("Could not assemble datapoint message", e);
+                throw new AukletException(assemblyFailure, e);
             }
             this.dataValue = this.msgpack.toByteArray();
         }
@@ -49,7 +49,7 @@ public class Datapoint {
                 this.msgpack.packMapHeader(1)
                         .packString(this.dataType).packByte(data);
             } catch (IOException e) {
-                throw new AukletException("Could not assemble datapoint message", e);
+                throw new AukletException(assemblyFailure, e);
             }
             this.dataValue = this.msgpack.toByteArray();
         }
@@ -68,7 +68,7 @@ public class Datapoint {
                 this.msgpack.packMapHeader(1)
                         .packString(this.dataType).packShort(data);
             } catch (IOException e) {
-                throw new AukletException("Could not assemble datapoint message", e);
+                throw new AukletException(assemblyFailure, e);
             }
             this.dataValue = this.msgpack.toByteArray();
         }
@@ -87,7 +87,7 @@ public class Datapoint {
                 this.msgpack.packMapHeader(1)
                         .packString(this.dataType).packInt(data);
             } catch (IOException e) {
-                throw new AukletException("Could not assemble datapoint message", e);
+                throw new AukletException(assemblyFailure, e);
             }
             this.dataValue = this.msgpack.toByteArray();
         }
@@ -106,7 +106,7 @@ public class Datapoint {
                 this.msgpack.packMapHeader(1)
                         .packString(this.dataType).packLong(data);
             } catch (IOException e) {
-                throw new AukletException("Could not assemble datapoint message", e);
+                throw new AukletException(assemblyFailure, e);
             }
             this.dataValue = this.msgpack.toByteArray();
         }
@@ -125,7 +125,7 @@ public class Datapoint {
                 this.msgpack.packMapHeader(1)
                         .packString(this.dataType).packBigInteger(data);
             } catch (IOException e) {
-                throw new AukletException("Could not assemble datapoint message", e);
+                throw new AukletException(assemblyFailure, e);
             }
             this.dataValue = this.msgpack.toByteArray();
         }
@@ -144,7 +144,7 @@ public class Datapoint {
                 this.msgpack.packMapHeader(1)
                         .packString(this.dataType).packFloat(data);
             } catch (IOException e) {
-                throw new AukletException("Could not assemble datapoint message", e);
+                throw new AukletException(assemblyFailure, e);
             }
             this.dataValue = this.msgpack.toByteArray();
         }
@@ -163,7 +163,7 @@ public class Datapoint {
                 this.msgpack.packMapHeader(1)
                         .packString(this.dataType).packDouble(data);
             } catch (IOException e) {
-                throw new AukletException("Could not assemble datapoint message", e);
+                throw new AukletException(assemblyFailure, e);
             }
             this.dataValue = this.msgpack.toByteArray();
         }
@@ -182,7 +182,7 @@ public class Datapoint {
                 this.msgpack.packMapHeader(1)
                         .packString(this.dataType).packBinaryHeader(data.length).addPayload(data);
             } catch (IOException e) {
-                throw new AukletException("Could not assemble datapoint message", e);
+                throw new AukletException(assemblyFailure, e);
             }
             this.dataValue = this.msgpack.toByteArray();
         }
@@ -201,7 +201,7 @@ public class Datapoint {
                 this.msgpack.packMapHeader(1)
                         .packString(this.dataType).packString(data);
             } catch (IOException e) {
-                throw new AukletException("Could not assemble datapoint message", e);
+                throw new AukletException(assemblyFailure, e);
             }
             this.dataValue = this.msgpack.toByteArray();
         }
@@ -220,7 +220,7 @@ public class Datapoint {
                 this.msgpack.packArrayHeader(1)
                         .packString(this.dataType).packArrayHeader(data.size());
             } catch (IOException e) {
-                throw new AukletException("Could not assemble datapoint message", e);
+                throw new AukletException(assemblyFailure, e);
             }
             this.dataValue = this.msgpack.toByteArray();
         }
@@ -239,7 +239,7 @@ public class Datapoint {
                 this.msgpack.packMapHeader(1)
                         .packString(this.dataType).packMapHeader(data.size());
             } catch (IOException e) {
-                throw new AukletException("Could not assemble datapoint message", e);
+                throw new AukletException(assemblyFailure, e);
             }
             this.dataValue = this.msgpack.toByteArray();
         }
